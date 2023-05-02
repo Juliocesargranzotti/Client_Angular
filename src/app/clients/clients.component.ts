947385ccdab35625e52e192e294989037baccf83
Juliocesargranzotti <jcgranzotti@gmail.com>
2023-05-02 17:25:10 -0300

    commit3

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { ClientService } from './../client.service';
+import { Component, OnInit } from '@angular/core';
+import { Client } from '../client';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent {
+export class ClientsComponent implements OnInit  {
+
+  clients: Client[] = [];
+
+  constructor (private ClientService: ClientService){}
+  
+  ngOnInit(): void {
+    this.loadClient();
+
+
+  }
+  loadClient() {
+    this.ClientService.getClients().subscribe(
+      {
+        next : data => this.clients = data
+      }
+    );
+  }
 
 }
